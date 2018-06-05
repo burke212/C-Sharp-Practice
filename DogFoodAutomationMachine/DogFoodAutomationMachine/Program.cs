5ce1330baaa18ae925985b1e4e3e32f0107f0205
burke212 <burke212@live.com>
2018-06-05 11:52:52 -0400

    Dog Food Auto Machine && Timer Example Projects
    Created 2 projects:
    - Dog Food Auto Machine
    - Timer Example
    
    Timer Example was a practice project so that I could learn & understand how the Timer class works.
    It performs 2 tasks: 1) show how a simple timer works (allows user to enter the Interval duration). 2) helped me understand how to use it in the other proejct.
    
    DFAM
    Every 10 seconds the prog checks the (random) weight of the bowl. If it is less than half the max weight, it is supposed to add food.
    
    Currently checks the weight every 10 sec, but isn't adding food.

---
+++ DogFoodAutomationMachine/DogFoodAutomationMachine/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace DogFoodAutomationMachine
+{
+    class Program
+    {
+        const int FULL = 10;
+        private int mTimer=0;
+        private bool M_CanReleaseFood = false;
+
+        static void Main(string[] args)
+        {
+            CanReleaseFood();
+        }
+
+        //static void Timer()
+        //{
+        //    /*
+        //     timer = 1hr
+        //     while(timer != 0)
+        //        timer - 1sec
+        //     timer alarm
+        //     */
+
+        //    Timer timer = new Timer();
+        //    timer.Elapsed += new ElapsedEventHandler();
+        //    //timer.Interval = 3600000;// 1 hour
+        //    timer.Interval = 5000;// 5 seconds
+        //    timer.Enabled = true;
+        //}
+
+        static double GetWeight()
+        {
+            Random rand = new Random();
+            // every 10 sec, check weight.
+            double w = rand.Next(0, FULL+1);
+
+            return w;
+        }
+
+        // Checks bowl weight every Interval.
+        static void CanReleaseFood()
+        {
+            Console.WriteLine("/nTimer has begun.");
+            Console.WriteLine("When finished, enter 'q' to quit the program.");
+            Timer aTimer = new Timer();// Creates aTimer object
+
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent); // OnTimedEvent is the event/ thing that is called after Interval has passed 
+            aTimer.Interval = 10000; // The duration to wait until the ElapsedEventHandler event is called.
+            aTimer.Enabled = true; // "Starts" the Interval
+
+            while (Console.Read() != 'q') ;
+        }
+        
+        // Determines if bowl needs filling by getting bowl weight. 
+        static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Program obj = new Program();
+            double weight = GetWeight();
+
+            Console.WriteLine("Weight: {0}", weight);
+
+            // Bowl is empty
+            if (weight == 0)
+            {
+                Console.WriteLine("Bowl is empty...");
+                
+                obj.M_CanReleaseFood = true;
+            }
+            // if bowl is less than half full
+            else if (weight < (FULL / 2))
+            {
+                Console.WriteLine("Bowl is less than half full...");
+                obj.M_CanReleaseFood = true;
+            }
+            // Bowl doesn't need filling
+            else
+            {
+                obj.M_CanReleaseFood = false;
+            }
+            ReleaseFood();
+        }
+
+        static void ReleaseFood()
+        {
+            Program obj = new Program();
+            double weight = GetWeight();
+
+            if(obj.M_CanReleaseFood == true)
+            {
+                // Release food. 
+                Console.WriteLine("Adding food");
+
+                // Add weight until it's half full
+                while(weight < (FULL / 2))
+                {
+                    ActivateRB(ref weight);
+                }
+            }
+        }
+
+        static void ActivateRB(ref double weight)
+        {
+            Random rand = new Random();
+            int nw;
+
+            nw = rand.Next(0, 11);
+            weight += nw;
+
+            Console.WriteLine("New Weight: {0}", weight);
+        }
+    }
+}
---
+++ TimerExample/TimerExample/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace TimerExample
+{
+    class Program
+    {
+        const int FULL = 10;
+        static void Main(string[] args)
+        {
+            int interval = 0;
+            int MM_Choice;
+            bool mCanReleaseFood = false;
+
+
+            Console.WriteLine("Choose an option to see the possibilities with the Timer class!");
+            Console.WriteLine("1. Basic Timer");
+            Console.WriteLine("2. Thread Timer");
+            MM_Choice = Int16.Parse(Console.ReadLine());
+
+            switch (MM_Choice)
+            {
+                case 1:
+                    Console.WriteLine("\nEnter a value for the timer Interval duration.");
+                    interval = Int32.Parse(Console.ReadLine()) * 1000;
+                    BasicTimer(interval);
+                    break;
+
+                case 2:
+                    ThreadTimer();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        //==================BASIC TIMER
+        private static void BasicTimer(int i)
+        {
+            Console.WriteLine("/nTimer has begun.");
+            Console.WriteLine("When finished, enter 'q' to quit the program.");
+            Timer aTimer = new Timer();// Creates aTimer object
+
+            aTimer.Elapsed += new ElapsedEventHandler(BasicOnTimedEvent); // OnTimedEvent is the event/ thing that is called after Interval has passed 
+            aTimer.Interval = i; // The duration to wait until the ElapsedEventHandler event is called.
+            aTimer.Enabled = true; // "Starts" the Interval
+
+            while (Console.Read() != 'q') ;
+        }
+
+        private static void BasicOnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(e.SignalTime);
+        }
+
+
+        //==================THREAD TIMER
+        private static void ThreadTimer()
+        {
+            Console.WriteLine("/nTimer has begun.");
+            Console.WriteLine("When finished, enter 'q' to quit the program.");
+            Timer aTimer = new Timer();// Creates aTimer object
+
+            aTimer.Elapsed += new ElapsedEventHandler(ThreadOnTimedEvent); // OnTimedEvent is the event/ thing that is called after Interval has passed 
+            aTimer.Interval = 10000; // The duration to wait until the ElapsedEventHandler event is called.
+            aTimer.Enabled = true; // "Starts" the Interval
+
+            while (Console.Read() != 'q') ;
+        }
+
+        private static void ThreadOnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Random rand = new Random();
+            // every 10 sec, check weight.
+            double w = rand.Next(0, FULL);
+
+            Console.WriteLine("Weight: {0}", w);
+
+            // Bowl is empty
+            if (w == 0)
+            {
+                Console.WriteLine("Bowl is empty...");
+            }
+            else if (w < (FULL / 2))// if bowl is less than half full
+            {
+                Console.WriteLine("Bowl is less than half full...");
+            }
+        }
+    }
+}
